e1d7a96008e2213957506e7a0c0139c1852a850d
stvpea <9432284+stvpea@users.noreply.github.com>
2021-09-06 19:01:02 +0100

    Change startup order to searcch for newer Creo first.

--- run_creo.bat
+++ run_creo.bat
@@ -1,3 +1,3 @@
 :: This command is in a separate file so that the start scripts may change it dynamically.
 
-start "" "C:\Program Files\PTC\Creo 3.0\M170\Parametric\bin\parametric.exe" || start "" "C:\Program Files\PTC\Creo 3.0\M190\Parametric\bin\parametric.exe" || start "" "C:\Program Files\PTC\Creo 3.0\M090\Parametric\bin\parametric.exe"
+start "" "C:\Program Files\PTC\Creo 3.0\M190\Parametric\bin\parametric.exe" || start "" "C:\Program Files\PTC\Creo 3.0\M170\Parametric\bin\parametric.exe" || start "" "C:\Program Files\PTC\Creo 3.0\M090\Parametric\bin\parametric.exe"
